f4ea247dee38ef20de0c28fc3ba0d0410ad00de3
FND <fnd@localhost.localdomain>
2014-12-07 16:48:22 +0100

    fixed gofmt complianced
    reluctantly

--- naveed/config.go
+++ naveed/config.go
@@ -3,16 +3,16 @@ package naveed
 import "github.com/BurntSushi/toml"
 
 type settings struct { // XXX: ambiguous names
-	Host string `toml:"host"`
-	Port int `toml:"port"`
-	PathPrefix string `toml:"path-prefix"`
-	ExternalRoot string `toml:"external-root"`
+	Host          string `toml:"host"`
+	Port          int    `toml:"port"`
+	PathPrefix    string `toml:"path-prefix"`
+	ExternalRoot  string `toml:"external-root"`
 	DefaultSender string `toml:"default-sender"`
-	UserIndex string `toml:"userindex"`
-	Sendmail string `toml:"sendmail"`
-	Tokens string `toml:"tokens"` // XXX: only required for testing
-	Preferences string `toml:"preferences"` // XXX: only required for testing
-	Templates string `toml:"templates"`   // XXX: only required for testing
+	UserIndex     string `toml:"userindex"`
+	Sendmail      string `toml:"sendmail"`
+	Tokens        string `toml:"tokens"`      // XXX: only required for testing
+	Preferences   string `toml:"preferences"` // XXX: only required for testing
+	Templates     string `toml:"templates"`   // XXX: only required for testing
 }
 
 var Config settings
